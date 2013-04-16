999d86410ff5a432706b41f5f1e58b26518a220e
bprzygoda <bprzygoda@gmail.com>
2013-04-16 08:34:54 +0000

    Another portion

--- bart-pack/config/bindings.el
+++ bart-pack/config/bindings.el
@@ -1,5 +1,6 @@
 ;; Place your bindings here.
 
+(define-key global-map (kbd "C-s") 'save-buffer)
 (define-key global-map (kbd "C-f") 'isearch-forward)
 (define-key global-map (kbd "C-o") 'find-file)
 ;;(define-key global-map (kbd "<escape>") 'keyboard-escape-quit)
@@ -10,6 +11,8 @@
 (define-key global-map (kbd "<f2>") 'split-window-below)
 (define-key global-map (kbd "<f3>") 'split-window-right)
 
-;; For example:
+(define-key global-map (kbd "C-b") 'ido-switch-buffer)
+(define-key global-map (kbd "C-n") 'win-switch-mode)
+
 ;;(define-key global-map (kbd "C-+") 'text-scale-increase)
 ;;(define-key global-map (kbd "C--") 'text-scale-decrease)
